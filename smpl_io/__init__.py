c0119ab9d64a63398fa4016c0b3e1d0e1e79068d
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2025-05-26 21:35:24 +0200

    rev clean

--- smpl_io/__init__.py
+++ smpl_io/__init__.py
@@ -4,7 +4,6 @@ from importlib.metadata import version
 
 package = "smpl_io"
 
-try:
-    __version__ = version(package)
-except:
-    __version__ = "unknown"
+__version__ = version("smpl")
+
+from smpl.io import *
